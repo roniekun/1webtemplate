64af3bfb94646be4cb5f92e219dbc956d40f6050
roniekun <roniebenitez01@gmail.com>
2023-10-20 19:03:21 +0800

    update

--- src/components/navigation/navlinks/index.jsx
+++ src/components/navigation/navlinks/index.jsx
@@ -11,7 +11,7 @@ const Navlinks = ({linkProps, containerProps}) => {
         key={index} 
         to={link.to}
         className={styles.link}
-        style={...linkProps}>
+        style={{...linkProps}}>
              {link.text}
     </NavLink>
     ))}
